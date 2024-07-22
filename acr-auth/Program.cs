29c6a36a0d559613bf5a71d533b90c7420cb261b
Yeming Liu <11371776+isra-fel@users.noreply.github.com>
2024-07-22 09:03:40 +0800

    test arc auth

---
+++ acr-auth/Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Containers.ContainerRegistry;
+using Azure.Identity;
+
+Uri endpoint = new Uri("https://***.azurecr.io");
+ContainerRegistryClient client = new ContainerRegistryClient(endpoint, new AzurePowerShellCredential(),
+    new ContainerRegistryClientOptions()
+    {
+        Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud
+    });
+
+Console.WriteLine(client.GetRepositoryNames().Count());
---
+++ acr-auth/README.md
@@ -0,0 +1,2 @@
+ACR is not purely AAD auth.
+It uses the AAD AT to get a RT and then an AT using data plane API.
